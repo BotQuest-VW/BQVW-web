120b956c20d244eef6950fa6e202e3f91a8e0d89
tamigld <thami.galves@outlook.com>
2023-09-12 19:57:03 -0300

    ajuste menu hamburguer

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -108,8 +108,19 @@ export default function Header(props:any) {
                     marginRight: '15px'
                   }}
                   onClick={abrirMenu}>
-                  {/* <MenuRoundedIcon className="icon_menu"/> */}
-                </div>
+                  
+                  <svg 
+                  className="icon_menu"
+                  style={{
+                    fill: "var(--branco)"
+                  }}
+                  xmlns="http://www.w3.org/2000/svg" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 512 512" width="16" height="16">
+                  <g>
+                    <path d="M480,224H32c-17.673,0-32,14.327-32,32s14.327,32,32,32h448c17.673,0,32-14.327,32-32S497.673,224,480,224z"/>
+                    <path d="M32,138.667h448c17.673,0,32-14.327,32-32s-14.327-32-32-32H32c-17.673,0-32,14.327-32,32S14.327,138.667,32,138.667z"/>
+                    <path d="M480,373.333H32c-17.673,0-32,14.327-32,32s14.327,32,32,32h448c17.673,0,32-14.327,32-32S497.673,373.333,480,373.333z"/>
+                  </g>
+                  </svg>                </div>
                 <p>Olá, {props.nome}!</p>
               </div>
               </>
