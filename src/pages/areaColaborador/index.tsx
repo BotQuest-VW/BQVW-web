969c123e3efc6b693c9e0270a12f67664660d7d6
tamigld <thami.galves@outlook.com>
2023-10-03 16:33:54 -0300

    responsividade ok
    responsividade finalizada Área Colaborador

--- src/pages/areaColaborador/index.tsx
+++ src/pages/areaColaborador/index.tsx
@@ -10,45 +10,11 @@ import CardNovidade from "../../components/cardNovidade";
 
 import api from "../../utils/api";
 import PerfilUsuario from "../../components/perfilUsuario";
+import DataAtual from "../../components/dataAtual";
 
 // import logado from '../../main';
 
 export default function AreaColaborador(props: any) {
-  // vvvvvvvvvvvvvv FUNÇÃO DISPLAY DATA ATUAL
-  const data = new Date();
-  const dia = String(data.getDate()).padStart(2, "0");
-  // const mes = String(data.getMonth() + 1).padStart(2, '0');
-  const ano = data.getFullYear();
-
-  const semana = [
-    "Domingo",
-    "Segunda-Feira",
-    "Terça-Feira",
-    "Quarta-Feira",
-    "Quinta-Feira",
-    "Sexta-Feira",
-    "Sábado",
-  ];
-  const mês = [
-    "Janeiro",
-    "Fevereiro",
-    "Março",
-    "Abril",
-    "Maio",
-    "Junho",
-    "Julho",
-    "Agosto",
-    "Setembro",
-    "Outubro",
-    "Novembro",
-    "Dezembro",
-  ];
-  const d = new Date();
-
-  const diaSemana = semana[d.getDay()];
-  const mesAtual = mês[d.getMonth()];
-  const dataAtual = diaSemana + ", " + dia + " de " + mesAtual + " de " + ano;
-  // ^^^^^^^^^^^^^^ FUNÇÃO DISPLAY DATA ATUAL
 
   const [tarefas, setTarefas] = useState<any[]>([]);
   const [tarefasDois, setTarefasDois] = useState<any[]>([]);
@@ -179,10 +145,7 @@ export default function AreaColaborador(props: any) {
           {visible == true ? (
             <>
               <section className="left">
-                <div>
-                  <h1>Visão Geral</h1>
-                  <span>{dataAtual}</span>
-                </div>
+                <DataAtual/>
                 <div>
                   <div className="card">
                     <h2
